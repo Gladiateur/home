176038da331f7e47bc1a5ff2bd24617b9a1e85a6
gladiateur <516222101@qq.com>
2021-03-13 16:56:09 +0800

    配置redis服务

--- src/main/java/gla/project/home/HomeApplication.java
+++ src/main/java/gla/project/home/HomeApplication.java
@@ -2,8 +2,10 @@ package gla.project.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"gla.project.redis"})
 public class HomeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/gla/project/redis/controller/RedisController.java
@@ -0,0 +1,23 @@
+package gla.project.redis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @GetMapping("/test1")
+    public void test(){
+        System.out.println("test redis haha");
+        stringRedisTemplate.opsForValue().set("springboot21","hello world");
+        String val = stringRedisTemplate.opsForValue().get("springboot21");
+        System.out.println(val);
+
+    }
+}
