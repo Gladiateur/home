8dc9a455d34a7490a16d7923ce4bb8b3209cf531
gladiateur <516222101@qq.com>
2021-03-13 15:51:36 +0800

    初始导入home

---
+++ src/test/java/gla/project/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package gla.project.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
